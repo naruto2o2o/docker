9fd4f46b001261647905c92f682be0e6eee0af97
naruto2o2o <251383431@qq.com>
2020-01-04 11:29:32 +0800

    c

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 docker run -d  \
 --name php-jin \
 -e TZ=Asia/Shanghai \
--v  /Users/jinzhiwen/Desktop/work:/work \
--v /Users/jinzhiwen/Desktop/mine/docker/php/:/usr/local/etc/  \
--v /Users/jinzhiwen/Desktop/mine/docker/log/:/log  \
+-v  /Users/jinzhiwen/dev/work:/work \
+-v /Users/jinzhiwen/dev/mine/docker/php/:/usr/local/etc/  \
+-v /Users/jinzhiwen/dev/mine/docker/log/:/log  \
 php-fpm:v1 "-R"
 
 docker run --name nginx-jin  -d \
 -e TZ=Asia/Shanghai \
--v /Users/jinzhiwen/Desktop/work:/work \
--v /Users/jinzhiwen/Desktop/mine/docker/nginx/conf/:/etc/nginx/ \
--v /Users/jinzhiwen/Desktop/mine/docker/log/:/log  \
+-v /Users/jinzhiwen/dev/work:/work \
+-v /Users/jinzhiwen/dev/mine/docker/nginx/conf/:/etc/nginx/ \
+-v /Users/jinzhiwen/dev/mine/docker/log/:/log  \
 -p 0.0.0.0:80:80 -p 0.0.0.0:443:443 \
 --link php-jin:php  nginx
 
